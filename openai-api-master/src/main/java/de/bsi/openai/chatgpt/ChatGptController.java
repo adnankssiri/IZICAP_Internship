3343ce82dfce142c30457acfbba8840d5cfa70d3
adnankssiri <86660736+adnankssiri@users.noreply.github.com>
2023-02-04 13:27:22 +0100

    Add files via upload

---
+++ openai-api-master/src/main/java/de/bsi/openai/chatgpt/ChatGptController.java
@@ -0,0 +1,52 @@
+package de.bsi.openai.chatgpt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import de.bsi.openai.FormInputDTO;
+import de.bsi.openai.OpenAiApiClient;
+import de.bsi.openai.OpenAiApiClient.OpenAiService;
+
+@Controller
+public class ChatGptController {
+	
+	private static final String MAIN_PAGE = "index";
+	
+	@Autowired private ObjectMapper jsonMapper;
+	@Autowired private OpenAiApiClient client;
+	
+	private String chatWithGpt3(String message) throws Exception {
+		var completion = CompletionRequest.defaultWith(message);
+		var postBodyJson = jsonMapper.writeValueAsString(completion);
+		var responseBody = client.postToOpenAiApi(postBodyJson, OpenAiService.GPT_3);
+		var completionResponse = jsonMapper.readValue(responseBody, CompletionResponse.class);
+		return completionResponse.firstAnswer().orElseThrow();
+	}
+	
+	@GetMapping(path = "/")
+	public String index() {
+		return MAIN_PAGE;
+	}
+	
+	@PostMapping(path = "/")
+	public String chat(Model model, @ModelAttribute FormInputDTO dto) {
+		try {
+			model.addAttribute("request", dto.prompt());
+			model.addAttribute("response", chatWithGpt3(dto.prompt()));
+		} catch (Exception e) {
+			model.addAttribute("response", "\"\\n\\nGluten sensitivity is a condition in which ingestion\n" +
+					"of gluten, a protein found in wheat, barley, and rye, causes a range of\n" +
+					"symptoms, such as abdominal pain, bloating, diarrhea, and constipation.\n" +
+					"People with gluten sensitivity have difficulty tolerating products which\n" +
+					"contain gluten, but are not diagnosed with celiac disease.\",.");
+		}
+		return MAIN_PAGE;
+	}
+	
+}
